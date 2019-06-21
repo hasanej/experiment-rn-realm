79f624ac913a5ac439e7eadab17a30dbf036ed48
Hasan El Jabir <eljabirhasan@gmail.com>
2019-06-21 15:21:33 +0700

    Add styles for Text custom component

---
+++ pages/components/Text/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  text: {
+    color: '#111825',
+    fontSize: 18,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+  }
+});
