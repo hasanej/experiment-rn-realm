0aaf8b4ad6b5cdf198859cc1d942e3fc68ea11bb
Hasan El Jabir <eljabirhasan@gmail.com>
2019-06-21 15:20:49 +0700

    Add styles for Button custom component

---
+++ pages/components/Button/styles.js
@@ -0,0 +1,16 @@
+import { Stylesheet } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#3A59B7',
+    color: '#FFF',
+    padding: 10,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+  },
+  text: {
+    color: '#FFF'
+  }
+});
